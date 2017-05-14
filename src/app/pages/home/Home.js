37a22257413555c04d1afe52d77ea60105a40482
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-05-15 01:06:45 +0200

    cleaned a bit more

--- src/app/pages/home/Home.js
+++ src/app/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 require('./home.scss');
-
+import image from '../../../img/profile-pic.jpg';
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
@@ -10,6 +10,7 @@ export default class Home extends React.Component {
         return (
             <div>
                 <h1>This is the home page!</h1>
+                <img src={image} />
             </div>
         )
     }
