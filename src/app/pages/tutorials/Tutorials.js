e59b29142fe53dfcc89078a29d44a3bdabcfbb02
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-05-15 00:34:04 +0200

    pulling tutorials from GitHub

--- src/app/pages/tutorials/Tutorials.js
+++ src/app/pages/tutorials/Tutorials.js
@@ -21,8 +21,20 @@ var showdown = require('showdown'),
 //     return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
 // }
 // marked.setOptions({ renderer });
-
+var tutorials = {
+    react: [
+        'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/React/1%20-%20Getting%20Started/_Article.md',
+    ],
+    angular2: [
+        'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/Angular/1%20-%20Getting%20Started/_Article.md',
+        'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/Angular/2%20-%20Application%20Architecture/_Article.md'
+    ],
+    fed: [
+        'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/FED/1%20-%20Project%20Structure/_Article.md'
+    ]
+}
 export default class Tutorials extends React.Component {
+    
     constructor(props) {
         super(props);
         this.getMarkdown();
@@ -34,14 +46,20 @@ export default class Tutorials extends React.Component {
     }
 
     getMarkdown() {
-        let tutUrl = 'https://github.com/bilo-io/tutorials/blob/master/Posts/React/1%20-%20Getting%20Started/_Article.md';
-        axios.get(tutUrl)
-            .then((response) => {
-                console.log(response);
-                text = response;
-            }).catch((error) => {
-                console.log(error);
-            })
+        let keys = Object.keys(tutorials);
+        keys.forEach((key) => {
+            tutorials[key].forEach((url) => {
+                let urlArray = url.split('/');
+                let id = `${key.toUpperCase()}: ${decodeURI(urlArray[urlArray.length - 2])}`;
+                axios.get(url)
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.error({ error });
+                    });
+            });
+        })
     }
 
     render() {
