4b3acb1886f1c802e64592acb07982eccd252113
Unknown <bilo.lwabona@gmail.com>
2017-05-15 16:52:26 +0200

    updates

--- src/app/pages/tutorials/Tutorials.js
+++ src/app/pages/tutorials/Tutorials.js
@@ -22,38 +22,98 @@ var showdown = require('showdown'),
 // }
 // marked.setOptions({ renderer });
 var tutorials = {
-    react: [
-        'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/React/1%20-%20Getting%20Started/_Article.md',
-    ],
-    angular2: [
-        'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/Angular/1%20-%20Getting%20Started/_Article.md',
-        'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/Angular/2%20-%20Application%20Architecture/_Article.md'
-    ],
-    fed: [
-        'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/FED/1%20-%20Project%20Structure/_Article.md'
-    ]
+    angular2: {
+        logo: 'https://angular.io/resources/images/logos/angular/angular.png',
+        tutorials: [
+            'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/Angular/1%20-%20Getting%20Started/_Article.md',
+            'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/Angular/2%20-%20Application%20Architecture/_Article.md'
+        ]
+    },
+    fed: {
+        logo: 'https://res.cloudinary.com/rapidweavercommunity/image/upload/s--38Ry9gwM--/c_fill,h_200,q_jpegmini,w_200/v1470026154/addons/icons/423554435.png',
+        tutorials: [
+            'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/FED/1%20-%20Project%20Structure/_Article.md'
+        ]
+    },
+    react: {
+        logo: 'http://www.jeremyzerr.com/sites/default/files/styles/large/public/field/image/react-logo-300-transparent.png',
+        tutorials: [
+            'https://raw.githubusercontent.com/bilo-io/tutorials/master/Posts/React/1%20-%20Getting%20Started/_Article.md',
+        ]
+    }
 }
 export default class Tutorials extends React.Component {
-    
+
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        this.outHtml = outHtml;
+        this.initState();
+    }
+
+    initState() {
+        this.setState(Object.assign({}, {
+            tutorials: tutorials,
+            data: [{
+                angular2: [
+                    {
+                        name: '',
+                        markdown: ''
+                    }
+                ]
+            }, {
+                fed: [
+                    {
+                        name: '',
+                        markdown: ''
+                    }
+                ]
+            }, {
+                react: [
+                    {
+                        name: '',
+                        markdown: ''
+                    }
+                ]
+            }
+
+            ]
+        }));
         this.getMarkdown();
     }
 
-    componentWillMount() {
-        this.outHtml = outHtml;
-        console.log(this);
+    render() {
+        return (
+            <div>
+                <h1>Tutorials</h1>
+                {this.tutorialSections()}
+                {/*<div dangerouslySetInnerHTML={this.getHtml()}></div>*/}
+            </div>
+        )
     }
 
     getMarkdown() {
         let keys = Object.keys(tutorials);
         keys.forEach((key) => {
-            tutorials[key].forEach((url) => {
+            tutorials[key].tutorials.forEach((url) => {
                 let urlArray = url.split('/');
                 let id = `${key.toUpperCase()}: ${decodeURI(urlArray[urlArray.length - 2])}`;
                 axios.get(url)
                     .then((response) => {
-                        console.log(response.data);
+                        let data = this.state.data;
+                        console.log(data, data[key]);
+                        this.setState(Object.assign(this.state, {
+                            data: Object.assign(this.state.data, {
+                                [key]: Object.assign(this.state.data[key], {
+                                    posts: Object.assign(this.state.data[key].posts, this.state.data[key].posts.push({
+                                        name: id,
+                                        markdown: response.data
+                                    }))
+                                })
+                            })
+                        }));
                     })
                     .catch((error) => {
                         console.error({ error });
@@ -62,14 +122,8 @@ export default class Tutorials extends React.Component {
         })
     }
 
-    render() {
-        return (
-            <div >
-            
-                <br />
-                <div dangerouslySetInnerHTML={this.getHtml()}></div>
-            </div>    
-        )
+    tutorialSections() {
+
     }
 
     getHtml() {
