66cb66bfe6f44b6245fce2972ab1f11a6af81c6b
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-07-20 01:53:19 +0200

    updates

--- src/app/components/profile/Profile.js
+++ src/app/components/profile/Profile.js
@@ -26,7 +26,7 @@ export default class Profile extends React.Component {
     render() {
         return (
             <div className='profile'>
-                    <h3>
+                <h3 style={{ color: 'white' }}>
                     <b>Bilo</b>
                     </h3>
                     <div>
