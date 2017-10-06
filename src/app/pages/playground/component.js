1289f9fa4fa2e4ab9d53f4a1e4b66f3c49d87305
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-10-06 16:46:42 +0200

    added draft-js: linkify & hashtag plugins

--- src/app/pages/playground/component.js
+++ src/app/pages/playground/component.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js'
+// DraftJS
+import { EditorState, RichUtils } from 'draft-js';
+// DraftJS Plugins
+import Editor from 'draft-js-plugins-editor';
+import createHashtagPlugin from 'draft-js-hashtag-plugin';
+import 'draft-js-hashtag-plugin/lib/plugin.css';
+import createLinkifyPlugin from 'draft-js-linkify-plugin';
+import 'draft-js-linkify-plugin/lib/plugin.css';
+// DraftJS END
 import './style.scss';
+import editorStyles from './styles-editor.scss';
 
+const hashtagPlugin = createHashtagPlugin();
+const linkifyPlugin = createLinkifyPlugin();
+
+const plugins = [
+    hashtagPlugin,
+    linkifyPlugin
+]
 export class Playground extends React.Component {
     constructor(props) {
         super(props)
@@ -18,17 +34,27 @@ export class Playground extends React.Component {
                 <div className="ws-card">
                     <button onClick={this._onBoldClick.bind(this)}>Bold</button>
                     <button onClick={this._onToggleCode.bind(this)}>{'< code />'}</button>
-                    <div className='custom-input'>
+                    <div className='custom-input' onClick={this.focus}>
                         <Editor
                             editorState={this.state.editorState}
                             handleKeyCommand={this.handleKeyCommand}
                             onChange={this.onChange}
+                            plugins={plugins}
+                            ref={(element) => { this.editor = element }}
                         />
                     </div>
                 </div>
             </div>
         ) : null
     }
+    focus = () => {
+        this.editor.focus();
+    };
+    onChange = (editorState) => {
+        this.setState({
+            editorState,
+        });
+    };
     handleKeyCommand(command) {
         console.log({ command })
         const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
