08661242970a4e7a973b5c3e5ac33e5650705d9d
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-10-06 14:59:18 +0200

    added draft.js to playground

--- src/app/pages/playground/component.js
+++ src/app/pages/playground/component.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './style.scss';
+import { Editor, EditorState } from 'draft-js'
 
-class Playground extends React.Component {
+export class Playground extends React.Component {
     constructor(props) {
         super(props)
     }
+    componentDidMount() {
+        this.setState({ editorState: EditorState.createEmpty()});
+        this.onChange = (editorState) => this.setState({ editorState })
+    }
     render() {
-        return (
+        return this.state ? (
             <div className='page page-padded'>
                 <h1>{this.props.title}</h1>
                 <div className="ws-card">
-                    <span data-text="true">a</span>
+                    <label>draft-js</label>
+                    <Editor editorState={this.state.editorState} onChange={this.onChange} />
                 </div>
             </div>
-        )
+        ) : null
     }
 }
 
