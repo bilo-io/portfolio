85e89f8abc6da869648475e902995a6958d37c25
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-10-06 15:51:38 +0200

    basic draft-js intro working

--- src/app/pages/playground/component.js
+++ src/app/pages/playground/component.js
@@ -1,25 +1,51 @@
 import React from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js'
 import './style.scss';
-import { Editor, EditorState } from 'draft-js'
 
 export class Playground extends React.Component {
     constructor(props) {
         super(props)
     }
     componentDidMount() {
-        this.setState({ editorState: EditorState.createEmpty()});
+        this.setState({ editorState: EditorState.createEmpty() });
         this.onChange = (editorState) => this.setState({ editorState })
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
     }
     render() {
         return this.state ? (
             <div className='page page-padded'>
                 <h1>{this.props.title}</h1>
                 <div className="ws-card">
-                    <label>draft-js</label>
-                    <Editor editorState={this.state.editorState} onChange={this.onChange} />
+                    <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+                    <button onClick={this._onToggleCode.bind(this)}>{'< code />'}</button>
+                    <div className='custom-input'>
+                        <Editor
+                            editorState={this.state.editorState}
+                            handleKeyCommand={this.handleKeyCommand}
+                            onChange={this.onChange}
+                        />
+                    </div>
                 </div>
             </div>
         ) : null
+    }
+    handleKeyCommand(command) {
+        console.log({ command })
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+        return 'not-handled';
+    }
+    _onBoldClick() {
+        this.onChange(RichUtils.toggleInlineStyle(
+            this.state.editorState,
+            'BOLD'
+        ));
+    }
+    _onToggleCode() {
+        this.onChange(RichUtils.toggleCode(this.state.editorState));
     }
 }
 
