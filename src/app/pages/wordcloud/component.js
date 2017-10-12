9dffaa78468b71872370b06ad252c0b5b807c2dd
Unknown <bilo.lwabona@gmail.com>
2017-10-12 12:33:30 +0200

    fuzzy-search working for word-clouds

--- src/app/pages/wordcloud/component.js
+++ src/app/pages/wordcloud/component.js
@@ -1,22 +1,86 @@
 import React, { Component, PropTypes } from 'react';
-import Search from 'bilo-ui';
+import { Search } from 'bilo-ui';
 import './style.scss';
+import FuzzySearch from 'fuzzy-search';
+import responses from './responses';
 
 export default class WordCloud extends Component {
-    static propTypes = {
-        // isActive: PropTypes.bool,
-        // hello: PropTypes.func.isRequired,
-        // index: PropTypes.number.isRequired,
-        // name: PropTypes.string.isRequired
-    }
     constructor(props) {
         super(props)
+        this.search = this.search.bind(this);
+        this.select = this.select.bind(this);
+    }
+    componentDidMount() {
+        this.searcher = new FuzzySearch(responses, ['text', 'tagline', 'person'], {
+            caseSensitive: false,
+        });
+
+        this.setState({
+            query: '',
+            suggestions: [],
+            searchResults: []
+        })
     }
     render() {
-        return (
+        return this.state ? (
             <div className='page page-padded'>
                 <h1>Word Cloud</h1>
+                <Search
+                    tag='wordcloud'
+                    placeholder='search responses...'
+                    search={this.search}
+                    select={this.select}
+                    suggestions={this.state.suggestions}
+                    suggestionsOn={false}
+                />
+                
+                <Suggestions 
+                    onSelect={this.select}
+                    suggestions={this.state.searchResults} />
+
             </div>
-        )
+        ) : null
     }
+    select(tag, item) {
+        console.log('Selected:', { item })
+    }
+    search(tag, query) {
+        if(!query || !query.length) {
+            this.setState({
+                ...this.state,
+                suggestions: [],
+                searchResults: []
+            })
+            return;
+        }
+        
+        let searchResult = this.searcher.search(query);
+        let results = searchResult.map((item) => {
+            return {
+                label: `${item.person}: "${item.text}"`
+            }
+        })
+        this.setState({
+            ...this.state,
+            suggestions: results,
+            searchResults: searchResult
+        })
+        console.log(searchResult)
+    }
+}
+
+export const Suggestions = (props) => {
+    return (
+        <div>{
+            (props.suggestions || []).map((response, index) => {
+                return (
+                    <div key={index} className='response-card' onClick={ () => props.onSelect('something', response)}>
+                        <label><b>{response.person}</b></label>
+                        <p style={{fontStyle: 'italic'}}><span>"{response.text}"</span></p>
+                        <p style={{color: '#999999'}}>~tagline: {response.tagline}</p>
+                    </div>
+                )
+            })
+        }</div>
+    )
 }
---
+++ src/app/pages/wordcloud/responses.js
@@ -0,0 +1,39 @@
+export const responses = [
+    {
+        text: 'This is my first response',
+        tagline: 'go with the flow',
+        person: 'Bilo Lwabona'
+    },
+    {
+        text: 'Dude, this is a hello world test for word clouds',
+        tagline: 'I\'ll give you hard tasks to start with :P',
+        person: 'Donovan Thomson'
+    },
+    {
+        text: 'Zappi is complicated as fuck',
+        tagline: 'use your brain!',
+        person: 'Brendon Mclean'
+    },
+    {
+        text: 'Hey, so what kind of thing would make your the world a better place for developers?',
+        tagline: 'support for all',
+        person: 'Brendon Mclean'
+    },
+    {
+        text: 'Developers, developers, developers, developers',
+        tagline: 'go with the flow',
+        person: 'Steve Balmer'
+    },
+    {
+        text: 'I turned the Apple fruit into the most expensive thing ever',
+        tagline: 'money money money',
+        person: 'Steve Jobs'
+    },
+    {
+        text: 'Apples .... hhmmm .... gravity',
+        tagline: 'science bitch',
+        person: 'Isaac Newton'
+    }
+];
+
+export default responses;
