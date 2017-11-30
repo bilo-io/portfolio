08d5ab1a3c67d427f54f3d64a482a4014252d41d
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-11-30 16:30:41 +0200

    signing in with github now...

--- src/app/pages/firebase/component.js
+++ src/app/pages/firebase/component.js
@@ -1,5 +1,9 @@
 import React, { Component, PropTypes } from 'react';
+import { Icon } from 'bilo-ui'
+import firebase from 'firebase'
 import './style.scss';
+
+var provider = new firebase.auth.GithubAuthProvider()
 
 export default class Firebase extends Component {
     static propTypes = {
@@ -10,21 +14,61 @@ export default class Firebase extends Component {
     constructor(props) {
         super(props)
     }
-    componentWillMount() {}
-    componentDidMount() {}
-    // updates
-    componentWillReceiveProps(nextProps) {}
-    shouldComponentUpdate(nextProps, nextState) {}
-    componentDidWillUpdate(nextProps, nextState) {}
-    componentDidUpdate(prevProps, prevState) {}
-    // unmounting & error handling
-    componentWillUnMount() {}
-    componentDidCatch(error, info) {}
+    
+    componentWillMount() { 
+        this.setState({
+            user: undefined,
+            username: undefined
+        })
+        this.initFirebase();
+    }
+    
+    componentDidMount() { }
+    
+    initFirebase() {
+        const config = {
+            apiKey: "AIzaSyCANPOz-hgWOugxob78o0pOQAURNQyrats",
+            authDomain: "deploy-queue.firebaseapp.com",
+            databaseURL: "https://deploy-queue.firebaseio.com",
+            projectId: "deploy-queue",
+            storageBucket: "",
+            messagingSenderId: "777160955980"    
+        }    
+        firebase.initializeApp(config)
+    }
+    signInWithGithub() {
+        console.log('signing in with github')
+        firebase.auth().signInWithPopup(provider).then((result) => {
+            // console.log('Signed in with github: ', JSON.stringify(result, true, 2), result)
+            this.setUser(result.additionalUserInfo)
+        }).catch((error) => {
+            console.log('Error signing in: ', error)
+        })
+    }
+
+    setUser(user) {
+        console.log(user)
+        this.setState({
+            ...this.state,
+            user: user,
+            username: user.username
+        }, () => { console.log(this.state) })
+    }
+
     //#endregion
     render() {
         return (
             <div className='page page-padded'>
                 <h1>Firebase</h1>
+                <button onClick={() => { this.signInWithGithub() }}>
+                    <Icon name='github' />
+                </button>
+                <br />
+                {this.state && this.state.user ? (
+                    <div>
+                        Signed in as: {this.state.username}
+                    </div>
+                ):null}
             </div>
         )
     }
