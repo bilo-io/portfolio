8068c4f5731651b98514e7af4c7b8874342b2a8e
Unknown <bilo.lwabona@gmail.com>
2017-12-01 00:11:00 +0200

    added user

--- src/app/pages/firebase/component.js
+++ src/app/pages/firebase/component.js
@@ -1,9 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import { Icon } from 'bilo-ui'
+import User from '../../components/user'
 import firebase from 'firebase'
 import './style.scss';
 
-var provider = new firebase.auth.GithubAuthProvider()
+var providerGithub = new firebase.auth.GithubAuthProvider()
+var providerGoogle = new firebase.auth.GoogleAuthProvider()
 
 export default class Firebase extends Component {
     static propTypes = {
@@ -17,9 +19,7 @@ export default class Firebase extends Component {
     
     componentWillMount() { 
         this.setState({
-            user: undefined,
-            username: undefined,
-            avatarUrl: undefined
+            user: undefined
         })
         this.initFirebase();
     }
@@ -38,41 +38,75 @@ export default class Firebase extends Component {
         firebase.initializeApp(config)
     }
     signInWithGithub() {
-        console.log('signing in with github')
-        firebase.auth().signInWithPopup(provider).then((result) => {
-            // console.log('Signed in with github: ', JSON.stringify(result, true, 2), result)
+        console.log('auth: github')
+        firebase.auth().signInWithPopup(providerGithub).then((result) => {
             this.setUser(result.additionalUserInfo)
         }).catch((error) => {
             console.log('Error signing in: ', error)
         })
+    }
+    signInWithGoogle() {
+        console.log('auth: google')
+        firebase.auth().signInWithPopup(providerGoogle).then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            console.log(result, token)
+            }).catch((error) => {
+              console.log()
+          });
     }
 
     setUser(user) {
         console.log(user)
         this.setState({
             ...this.state,
-            user: user,
-            username: user.username,
-            avatarUrl: user.profile.avatar_url
+            user: {
+                username: user.username,
+                fullname: user.profile.name,
+                avatarUrl: user.profile.avatar_url
+            }
         }, () => { console.log(this.state) })
     }
 
     //#endregion
     render() {
+        let signInOptions = [
+            <div key='github_auth' className='signin-button' onClick={() => { this.signInWithGithub() }}>
+                <Icon name='github' style={{marginRight: '1em'}}/>
+                <span>Sign in with Github</span>
+            </div>,
+            <div key='google_auth' className='signin-button' onClick={() => { this.signInWithGoogle() }}>
+                <Icon name='google' style={{marginRight: '1em'}}/>
+                <span>Sign in with Google</span>
+            </div>
+        ]
+
+        let users = [{
+                username: 'Lutando',
+                fullname: 'Lutando Ngqakaza',
+                avatarUrl: 'https://avatars1.githubusercontent.com/u/1838141?s=460&v=4'
+            },{
+                username: 'henk',
+                fullname: '',
+                avatarUrl: 'https://avatars2.githubusercontent.com/u/13061442?s=460&v=4'
+            },{
+                username: 'circlingthesun',
+                fullname: 'Rickert Mulder',
+                avatarUrl: 'https://avatars0.githubusercontent.com/u/1028632?s=460&v=4'
+            }
+        ]
         return (
             <div className='page page-padded'>
                 <h1>Firebase</h1>
-                <button onClick={() => { this.signInWithGithub() }}>
-                    <Icon name='github' />
-                </button>
-                <br />
-                <br />
+                {this.user
+                    ? null
+                    : signInOptions
+                }
+                {users.map( (user) => {
+                    return <User key={user.username} user={user} />
+                })}
                 {this.state && this.state.user ? (
-                    <div>
-                        {this.state.username}
-                        <br />
-                        <img width='64px' style={{borderRadius: '50%', width: '4em', height: 'auto'}} src={this.state.avatarUrl} />
-                    </div>
+                    <User user={this.state.user} />
                 ):null}
             </div>
         )
