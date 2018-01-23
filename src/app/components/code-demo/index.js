43156400c718e1acce343754cf21a0c74efb67b9
Unknown <bilo.lwabona@gmail.com>
2018-01-23 10:31:14 +0200

    added basic html syntaxHighligting

--- src/app/components/code-demo/index.js
+++ src/app/components/code-demo/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Icon } from 'bilo-ui'
 import './style.scss'
+// import Highlight from 'react-hljs'
+import Highlight from 'react-highlight.js'
 import jsxToString from 'jsx-to-string'
 import reactElementToJSXString from 'react-element-to-jsx-string';
 import ReactElementToString from 'react-element-to-string'
@@ -24,8 +26,14 @@ class CodeDemo extends React.Component {
     
     componentWillMount() {
         this.setState({
-            isOpen: false
+            isOpen: true
         })
+    }
+
+    componentDidMount() {
+        const { code } = this.props;
+        console.log({code})
+        // console.log(ReactElementToString(code))
     }
     
     toggle() {
@@ -38,12 +46,23 @@ class CodeDemo extends React.Component {
     
     render() {
         const { isOpen } = this.state;
-        return this.state ? (
+        const { code } = this.props;
+        return this.state && code ? (
             <div className='ws-card'>
                 <div className='code-block'>
-                    <span className='toggle' onClick={() => this.toggle()}>
-                        <Icon name='code' /> {isOpen ? 'hide code' : 'code'}
-                    </span>
+                    <div className='toggle' onClick={() => this.toggle()}>
+                        <Icon name='code' /> {isOpen ? 'hide' : 'code'}
+                    </div>
+                    <br />
+                    <div>
+                        {
+                            code && isOpen
+                            ? <Highlight language={'html'}>
+                                {ReactElementToString(code)}
+                            </Highlight> 
+                            :null
+                        }
+                    </div>
                 </div>
                 { this.props.children }
             </div>
--- src/app/pages/xui/demos/inputs.js
+++ src/app/pages/xui/demos/inputs.js
@@ -1,61 +1,86 @@
 import React, { Component } from 'react'
-import {Button, Icon, Loader, LoaderType} from 'bilo-ui';
+import {If, Button, Icon, Loader, LoaderType} from 'bilo-ui';
 import CodeDemo from '../../../components/code-demo'
 
 export default class Inputs extends Component {
     render() {
+        const textInputs = <div>
+                <div>
+                    <label>text</label>
+                    <input type='text' />
+                </div>
+                <div>
+                    <label>number</label>
+                    <input type='number' />
+                </div>
+                <div>
+                    <label>email</label>
+                    <input type='email' />
+                </div>
+                <div>
+                    <label>password</label>
+                    <input type='password' />
+                </div>
+                <div>
+                    <label>tel</label>
+                    <input type='tel' />
+                </div>
+            </div>
+        const dateTimeInputs = <div>
+                <div>
+                    <label>date</label>
+                    <input type='date' />
+                </div> 
+                <div>
+                    <label>time</label>
+                    <input type='time' />
+                </div>
+                <div>
+                    <label>datetime</label>
+                    <input type='datetime-local' />
+                </div>
+            </div>
+        const otherInputs = <div>
+                    <If isTrue={ true }>
+                        <div>
+                            <label>color</label>
+                            <input type='color' />
+                        </div>
+                    </If>
+                    
+                    <div>
+                        <label>range</label>
+                        <input type='range' />
+                    </div>
+
+                    <div>
+                        <label>checkboxes</label>
+                        <input type='checkbox' />
+                        <input type='checkbox' />
+                        <input type='checkbox' />
+                    </div>
+
+                    <div>
+                        <label>radio buttons</label>
+                        <input type='radio' />
+                        <input type='radio' />
+                        <input type='radio' />
+                    </div>
+            </div>
         return (
             <div className='ws-card'>
                 <h2>Inputs</h2>
-                <CodeDemo>
-                    <div>
-                        <label>text</label>
-                        <input type='text' />
-                    </div>
-                    <div>
-                        <label>number</label>
-                        <input type='number' />
-                    </div>
+                <CodeDemo code={ textInputs }>
+                    {textInputs}
                 </CodeDemo>
 
-                <CodeDemo>
-                    <div>
-                        <label>date</label>
-                        <input type='date' />
-                    </div> 
-                    <div>
-                        <label>time</label>
-                        <input type='time' />
-                    </div>
-                    <div>
-                        <label>datetime</label>
-                        <input type='datetime-local' />
-                    </div>
+                <CodeDemo code={ dateTimeInputs }>
+                    {dateTimeInputs}
                 </CodeDemo>
 
-                <div>
-                    <label>color</label>
-                    <input type='color' />
-                </div>
-                
-                <div>
-                    <label>range</label>
-                    <input type='range' />
-                </div>
-
-                <div>
-                    <label>checkboxes</label>
-                    <input type='checkbox' />
-                    <input type='checkbox' />
-                    <input type='checkbox' />
-                </div>
-
-                <div>
-                    <label>radio buttons</label>
-                    <input type='radio' />
-                    <input type='radio' />
-                    <input type='radio' />
-                </div>
+                <CodeDemo code={ otherInputs }>
+                    { otherInputs }
+                </CodeDemo>
             </div>
         )
     }
