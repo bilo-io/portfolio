ea7cc529ea6632ec8303e99bd563f572ec760842
Bilo Lwabona <bilo.lwabona@gmail.com>
2018-02-04 20:53:55 +0200

    generating docs

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import store from './store';
 import {Route, Switch, Link} from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Async from 'react-code-splitting'
 // Components
-import {AppTopBar, AppSidenav, AppBody} from 'bilo-ui';
+import { AppTopBar, AppSidenav, AppBody } from 'bilo-ui';
+import Home from './pages/home'
 // Containers
 
 require('../app.scss');
@@ -17,45 +18,41 @@ export class App extends React.Component {
         let {sidenav} = this.state;
         return this.state
             ? (
-                <Router>
-                    <div className='fullscreen'>
-                        <AppTopBar>
-                            <img
-                                src='https://raw.githubusercontent.com/bilo-io/resources/master/logo/react.png'
-                                onClick={() => this.toggleSidenav()}
-                                width='48'/>
-                            <Link to="/">bilo.io</Link>
-                        </AppTopBar>
-                        <AppBody>
-                            <AppSidenav
-                                isOpen={this.state.sidenav.isOpen}
-                                style={{position: 'fixed', top: '3em'}}>
-                                {sidenav.items.map((page) => {
-                                        return <Link
-                                            key={page.link}
-                                            to={page.link}
-                                            className='sidenav-link'
-                                            onClick={() => {
-                                            this.toggleSidenav()
-                                        }}>
-                                            {page.name.toUpperCase()}
-                                        </Link>
-                                    })}
-                            </AppSidenav>
-                            <Switch>
-                                <Route exact path="/"
-                                    component={() => <Async load={import(/* webpackChunkName: "Home"*/ './pages/home')} />} />
-                                <Route exact path="/tutorials" 
-                                    component={() => <Async load={import(/* webpackChunkName: "Tutorials"*/ './pages/tutorials')} />}/>
-                                <Route exact path="/tutorials/:tutorialId"
-                                    component={() => <Async load={import(/* webpackChunkName: "MarkdownReader"*/ './pages/markdown-reader')} />} />
-                                <Route path="*"
-                                    component={() =>
-                                        <Async load={import(/* webpackChunkName: "NotFound"*/ './pages/not-found')} />} />
-                            </Switch>
-                        </AppBody>
-                    </div>
-                </Router>
+                <div className='fullscreen'>
+                    <AppTopBar>
+                        <img
+                            src='https://raw.githubusercontent.com/bilo-io/resources/master/logo/react.png'
+                            onClick={() => this.toggleSidenav()}
+                            width='48'/>
+                        {/* <Link to="/">bilo.io</Link> */}
+                    </AppTopBar>
+                    <AppBody>
+                        <AppSidenav
+                            isOpen={this.state.sidenav.isOpen}
+                            style={{
+                            position: 'fixed',
+                            top: '3em'
+                        }}>
+                            {sidenav
+                                .items
+                                .map((page) => {
+                                    return <Link
+                                        key={page.link}
+                                        to={page.link}
+                                        className='sidenav-link'
+                                        onClick={() => {
+                                        this.toggleSidenav()
+                                    }}>
+                                        {page
+                                            .name
+                                            .toUpperCase()}
+                                    </Link>
+                                })}
+                        </AppSidenav>
+
+                        <Home/>
+                    </AppBody>
+                </div>
             )
             : null
     }
@@ -68,13 +65,8 @@ export class App extends React.Component {
                         link: '/',
                         name: 'bilo'
                     }
-                    // }, {
-                    //     link: '/contact',
-                    //     name: 'contact'
-                    // }, {
-                    //     link: '/firebase',
-                    //     name: 'firebase'
-                    // }
+                    // }, {     link: '/contact',     name: 'contact' }, {     link: '/firebase',
+                    //  name: 'firebase' }
                 ]
             }
         })
---
+++ webpack.static.config.js
@@ -0,0 +1,57 @@
+const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Visualizer = require('webpack-visualizer-plugin');
+const path = require('path');
+const DIST = path.resolve(__dirname, 'demo/');
+const SRC = path.resolve(__dirname, 'src/');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var config = {
+    entry: {
+        main: SRC + '/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './docs/'),
+        filename: 'demo.js',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.(css|scss)$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }, {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file-loader?name=assets/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html', inject: 'body' }),
+        new CopyWebpackPlugin([{
+            from: SRC + '/img/*',
+            to: DIST + '/assets/*'
+        }]),
+        new StaticSiteGeneratorPlugin({
+            paths: [
+              '/',
+              '/bilo'
+            ],
+            locals: {
+              greet: 'Hello'
+            }
+          })
+    ],
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    }
+};
+
+module.exports = config;
