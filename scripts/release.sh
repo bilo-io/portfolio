33513d4e90b90e87ff7a22ab023243dda8bcbe37
Bilo Lwabona <bilo.lwabona@gmail.com>
2018-02-23 00:29:29 +0200

    -> Release: (2018-02-23) => 'testing release script'

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,17 +2,21 @@
 # . /usr/local/bin/.shell/sh/utils/colors.sh
 . ./scripts/colors.sh
 function unsavedWarning() {
-git status 
+git status
 echoColor 'yellow' "
-warning: commit your changes before releasing!
-  "
-  exit 0
+add untracked files to release? (y/n): "
+read ans
+case "$ans" in
+  "y") ;;
+  "n") echoColor 'yellow' ' -> aborting release';exit 0;;
+  *) echoColor 'yellow' " -> unknown command: $ans"; exit 0;;
+esac
 }
 timestamp=$(date +%Y-%m-%d)
 echoColor 'red' "[-] docs"
 rm -rf "./docs"
 echoColor 'cyan-l' "[.] building ..."
-# npm run build:demo
+npm run build:demo
 echoColor 'green-l' "[+] ./docs"
 echoColor 'green-l' "[+] => git"
 git diff-index --quiet HEAD -- || unsavedWarning
@@ -21,5 +25,5 @@ echo -n "release message: "
 read commitMsg
 msg="-> Release: ($timestamp) => '$commitMsg'"
 echo $msg
-# git commit -m "$msg"
-# git push origin master
+git commit -m "$msg"
+git push origin master
