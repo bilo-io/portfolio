06ce6c056031a177299aaf4de33315a3b743a68c
Bilo Lwabona <bilo.lwabona@gmail.com>
2018-02-23 00:19:20 +0200

    added git warning to release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # . /usr/local/bin/.shell/sh/utils/colors.sh
 . ./scripts/colors.sh
+function unsavedWarning() {
+git status 
+echoColor 'yellow' "
+warning: commit your changes before releasing!
+  "
+  exit 0
+}
 timestamp=$(date +%Y-%m-%d)
 echoColor 'red' "[-] docs"
 rm -rf "./docs"
@@ -8,9 +15,7 @@ echoColor 'cyan-l' "[.] building ..."
 # npm run build:demo
 echoColor 'green-l' "[+] ./docs"
 echoColor 'green-l' "[+] => git"
-git diff-index --quiet HEAD -- || git status && echoColor 'yellow' "
- warning: commit your changes before releasing!
-" && exit 0;
+git diff-index --quiet HEAD -- || unsavedWarning
 git add -A
 echo -n "release message: "
 read commitMsg
