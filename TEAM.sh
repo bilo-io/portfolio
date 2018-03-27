fb97e9cf478879a0d3e57bd5bde7b38fc92718c4
Bilo Lwabona <bilo.lwabona@gmail.com>
2018-03-28 00:25:02 +0200

    updated script

--- TEAM.sh
+++ TEAM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gitBase="bilo-io/"
+gitBase="https://github.com/bilo-io/"
 
 repos=(
   "bilo-bio"
@@ -8,7 +8,42 @@ repos=(
   "bilo-cli"
 )
 
+function printRepos {
 for index in ${!repos[*]}
 do
 echo $gitBase${repos[$index]}
 done
+}
+
+function delGitBranch {
+  currentRepo=$(basename `git rev-parse --show-toplevel`)
+  currentBranch=$(git rev-parse --abbrev-ref HEAD)  
+  
+  git checkout master
+  git branch -D $currentBranch
+  git push -d origin $currentBranch
+}
+function newGitBranch {
+  currentRepo=$(basename `git rev-parse --show-toplevel`)
+  currentBranch=$(git rev-parse --abbrev-ref HEAD)
+
+  branchName=$1
+  echo $currentRepo
+  echo "\\"
+  echo " +-+-+>> $currentBranch"
+  git stash
+  git checkout master
+  git pull
+  git checkout -b $branchName
+  # git add -A
+  # git commit -m "created branch: $branchName"
+  git push origin $branchName
+  git checkout $currentBranch
+  git stash pop
+}
+echo -n -e "create branch: "
+read customBranchName
+
+printRepos
+# newGitBranch $customBranchName
+delGitBranch "yoyo"
